073ba4830fff17846372927e1c1237b826269f5b
nightfallenxyz <160358800+nightfallenxyz@users.noreply.github.com>
2025-01-30 19:30:36 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'iptv-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/icon.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(response => response || fetch(e.request))
+    );
+});
